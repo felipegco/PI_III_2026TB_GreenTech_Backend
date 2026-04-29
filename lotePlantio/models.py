67c217b09ef356ce677ead22016dbadb2dcff63b
Felipe Giacomini Cocco <felipegco@proton.me>
2026-04-29 19:43:53 -0300

    feat: adiciona novos campos ao modelo "LotePlantio" e ajusta choices em "status", modifica choices no modelo "Funcionario"

--- lotePlantio/models.py
+++ lotePlantio/models.py
@@ -4,21 +4,32 @@ from estufa.models import Estufa
 
 class LotePlantio(models.Model):
     class StatusPlantio(models.TextChoices):
+        EM_ESTOQUE = 'ES', 'Em Estoque'
+        ESTOQUE_BAIXO = 'BX', 'Estoque Baixo'
+        DISPONIVEL = 'DI', 'Disponível'
         ATIVO = 'AT', 'Ativo'
         COLHIDO = 'CO', 'Colhido'
         PERDIDO = 'PE', 'Perdido'
 
     estufa = models.ForeignKey(Estufa, on_delete=models.CASCADE)
-    cultura = models.CharField(max_length=100)
-    data_plantio = models.DateField()
 
-    # ATENÇÃO AQUI: Adicione o .choices
+    cultura = models.CharField(max_length=100)
+    fornecedor = models.CharField(max_length=150, blank=True, null=True)
+
+    data_plantio = models.DateField()
+    validade = models.DateField(blank=True, null=True)
+
     status = models.CharField(
         max_length=2,
         choices=StatusPlantio.choices,
         default=StatusPlantio.ATIVO
     )
+
+    custo = models.FloatField(default=0.0)
     custo_total = models.FloatField(default=0.0)
+
+    quantidade = models.FloatField(default=0.0)
+    unidade = models.CharField(max_length=20, blank=True, null=True)
 
     class Meta:
         db_table = 'lote_plantio'
