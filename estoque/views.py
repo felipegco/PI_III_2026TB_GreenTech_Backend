c5b06104183892f034022eac202ac21229f27677
Felipe Giacomini Cocco <felipegco@proton.me>
2026-06-01 13:42:36 -0300

    feat: Adiciona restrições de acesso à exclusão de registros no histórico com base em permissões de usuário

--- estoque/views.py
+++ estoque/views.py
@@ -44,3 +44,16 @@ class EstoqueViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        is_superuser = request.user.is_superuser
+        is_gerente = request.user.groups.filter(name__iexact='gerente').exists()
+        is_admin = request.user.groups.filter(name__iexact='admin').exists()
+
+        if not (is_superuser or is_gerente or is_admin):
+            return Response(
+                {"error": "Acesso negado. Apenas gerentes e administradores podem eliminar registos do histórico."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        return super().destroy(request, *args, **kwargs)
